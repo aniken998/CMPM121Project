eeddedf6b318d3f51c3bf64ef2291fd8fe784a83
aniken998 <enzhou@ucsc.edu>
2020-07-20 20:28:46 -0700

    add UI and made custom particles

---
+++ Source/CMPM121Project/MyLevelScriptBlueprint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LevelScriptBlueprint.h"
+#include "MyLevelScriptBlueprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CMPM121PROJECT_API UMyLevelScriptBlueprint : public ULevelScriptBlueprint
+{
+	GENERATED_BODY()
+	
+};
